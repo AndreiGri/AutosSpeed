5d987b20c18987eb6a8448e4c218e63dbf9ae38e
Andrei Gridin <andreigridin918@gmail.com>
2023-09-01 22:07:41 +0500

    Добавьте файлы проекта.

---
+++ AutosSpeed/AutosSpeed.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    int distance = 200;
+    int time = 2;
+    int speed = 0;
+
+    cout << "Введите скорость автомобиля: ";
+    cin >> speed;
+
+    if (speed * time < distance) {
+        cout << "Вы проехали " << speed * time << " километров." << endl;
+    }
+    else {
+        cout << "Вы приехали!" << endl;
+    }
+
+    // Задержка консольного окна:
+    system("pause > nul");
+    return 0;
+}
